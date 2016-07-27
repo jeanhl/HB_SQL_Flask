0a42d2c5ca5b54dda9575b464741b662d199b21c
Hackbright Student <info@hackbrightacademy.com>
2016-07-26 17:13:19 -0700

    Able to add students to database.

--- hackbright-web.py
+++ hackbright-web.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, session
 
 import hackbright
 
@@ -22,6 +22,30 @@ def get_student_form():
     """Show a form for searching for a student."""
 
     return render_template("student_search.html")
+
+@app.route("/student-add-form")
+def display_student_add_form():
+    """Add a student."""
+    #first, last, github = request.form.get()
+
+    return render_template("student_add.html")
+
+@app.route("/student-add", methods=['POST'])
+def handle_student_add():
+    """Add a student."""
+    first = request.form.get("firstname")
+    last = request.form.get("lastname")
+    github = request.form.get("github")
+
+    hackbright.make_new_student(first, last, github)
+
+    return render_template("student_info.html", first=first, last=last, github=github)
+
+# @app.route("/confirm-add")
+# def confirm_add_student():
+# """Confirm that the user has added a student successfully."""
+
+# return render_template("student_info.html", first=first, last=last, github=github)
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
